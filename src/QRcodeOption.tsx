a988f65677e51e8183719609cfc4ff12a256a0ae
seifeldin1 <seif1442004@gmail.com>
2024-06-09 13:16:01 +0300

    updated qr

--- src/QRcodeOption.tsx
+++ src/QRcodeOption.tsx
@@ -1,37 +1,38 @@
 import React, { FC, useState } from "react";
-import { Link } from "react-router-dom";
-import { TextField, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Box } from "@mui/material";
 import QRCode from "qrcode.react";
 
-const QRPayment:React.FC=()=>{
-    const paymentURL = "https://www.paypal.com/eg/home"
+const QRPayment: FC = () => {
+    const paymentURL = "https://www.paypal.com/eg/home";
+    const [scanned, setScanned] = useState(false);
+    const navigate = useNavigate();
 
-    const [scanned , setScanned]= useState(false);
-
-    const handleScan=()=>{
+    const handleScan = () => {
         setScanned(true);
-    }
+        navigate("/home/userInfo/payment/success");
+    };
+
+   
+   /* React.useEffect(() => {
+        const timer = setTimeout(() => {
+            handleScan();
+        }, 5000); // Simulate 5 seconds delay
+
+        return () => clearTimeout(timer);
+    }, []);*/
 
     return (
         <div className="flex items-center justify-center w-xl h-screen">
             <div className="h-96 w-96 border-b-8 m-5 p-5 rounded-lg border-white bg-white">
-                <Box sx={{display:'flex' , flexDirection:'column' , alignItems:'center' , justifyContent:'center'}}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                     <h1 className="text-red-700 text-2xl font-mono font-bold">Scan To Pay</h1>
-                    <p className="text-red-500 font-mono font-bold">Note this is a paypal payement , so you will be redirected to paybal after scanning for payment</p>
-                    <QRCode value={paymentURL} size={220} className="m-5 p-10" onClick={handleScan}></QRCode>
-                    {scanned && (
-                        <Link to="/home/userInfo/payment/success">
-                            <button className="bg-green-300 flex items-center justify-content text-red-700 w-60 h-20 rounded-lg m-5 p-5">
-                                Proceed
-                            </button>
-                        </Link>
-                     )}
+                    <p className="text-red-500 font-mono font-bold">Note this is a PayPal payment, so you will be redirected to PayPal after scanning for payment</p>
+                    <QRCode value={paymentURL} size={220} className="m-5 p-10" onClick={handleScan} />
                 </Box>
             </div>
-
         </div>
-    )
-}
-
+    );
+};
 
 export default QRPayment;
